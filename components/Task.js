65fd233c3540fc98b64a4fbfcf4ac8d730a45a2b
joseliraa <josmliraa@gmail.com>
2022-06-22 01:52:49 -0600

    branch develop - first commit

---
+++ components/Task.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default Task = (props) => {
+    return(
+        <View>
+            <Text>{props.text}</Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+
+});
