e6bfaec3eb98aecaec5dac8ed5b751b3e3eb3e7c
joseliraa <josmliraa@gmail.com>
2022-06-23 18:37:24 -0600

    adding task done, remove and select completed task with errors

--- App.js
+++ App.js
@@ -1,26 +1,94 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  ScrollView,  
   StyleSheet,
   Text,
-  useColorScheme,
+  KeyboardAvoidingView,
   View,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  Alert,
 } from 'react-native';
 import Task from './components/Task';
 
 export default function App(){
+  const [task, setTask] = useState();
+  const [taskItem, setTaskItem] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    if(task == null ){
+      alert("Please type something");
+    }else if(taskItem.length <= 9){
+      setTaskItem([...taskItem,task])
+      setTask(null);
+    }else{
+      alert("You can only enter 10 task at a time!")
+}}
+
+function setIsSelected(index, value){
+  let data = []
+  
+  for(let i=0; i < taskItem.length; i++){
+    if(index === i){
+      data.push({...taskItem, isSelected: value})
+      console.log("estoy aqui 1",index, value);      
+    }else{
+      data.push(taskItem[i])    
+      console.log("estoy aqui 2",index, value);  
+    }
+  }
+}
+
+function deleteItem(index){
+  Alert.alert(
+      "Delete Item",
+      "Are you sure you want to delete this item?",
+      [
+          {
+              text: "Cancel",
+              style: "Cancel"
+          },
+          {
+              text: "Yes", onPress: () => {
+              console.log("estoy aqui 3",index); 
+              let itemsCopy = [...taskItem];
+              itemsCopy.splice(index,1);
+              setTaskItem(itemsCopy);
+              }
+          }
+      ])
+}
+
   return (
     <View style={styles.container}>
-      <View style={styles.taskContainer}>
+      <ScrollView style={styles.taskContainer}>
         <Text style={styles.titleSection}>Today's tasks</Text>
-
         <View style={styles.items}>
-          <Task text= {'first task'} />
-          <Task text= {'second task'} />
+          {            
+            taskItem.map((item, index) => {
+              return <Task key = {index} text = {item} setIsSelected={setIsSelected} deleteItem={deleteItem}/>
+            }).reverse()
+          }
         </View>
-      </View>
+      </ScrollView>
+
+
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style = {styles.typedTaskWrapper}      
+      >
+        <TextInput style = {styles.input} 
+        placeholder = {'Write a task'} value = {task} onChangeText={text => setTask(text)}>          
+        </TextInput>
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style = {styles.buttonWrapper}>
+            <Text style = {styles.buttonText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -30,14 +98,44 @@ const styles = StyleSheet.create({
     backgroundColor: '#E8EAED',
   },
   taskContainer:{
-    paddingTop: 80,
+    paddingTop: 70,
     paddingHorizontal: 20,    
   },
   titleSection:{
     fontWeight: 'bold',
-    fontSize: 20
+    fontSize: 20,    
   },
   items:{
+    marginTop: 35,
+  },
+  typedTaskWrapper:{
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  buttonWrapper:{
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  buttonText:{
 
   },
 });
